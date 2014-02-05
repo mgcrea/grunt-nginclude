32205c70283dce4256866c4f8d7ca0a86ae95ea7
Guilherme Berger <guilherme.berger@gmail.com>
2014-02-05 16:37:06 -0200

    fix(typo): use === instead of ==

--- tasks/nginclude.js
+++ tasks/nginclude.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
         var tags = $('ng-include, [ng-include]');
 
         // If we don't find any more ng-include tags, we're done.
-        if (tags.length == 0) {
+        if (tags.length === 0) {
           return $.html();
         }
 
