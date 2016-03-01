5d0bf1fcacbdd8cf7994cd3e0a22408d8219b176
maiap <pedro.maia@blip.pt>
2016-03-01 23:51:22 +0000

    fix indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,29 @@
 module.exports = function (grunt) {
-  'use strict';
-  var path = require('path');
+    'use strict';
+    var path = require('path');
 
-  grunt.registerTask('release', 'Release and publish new `grunt-nginclude` version', function (releaseType) {
-    var RELEASE_TYPES = ['major', 'minor', 'patch'];
+    grunt.registerTask('release', 'Release and publish new `grunt-nginclude` version', function (releaseType) {
+        var RELEASE_TYPES = ['major', 'minor', 'patch'];
 
-    if (!releaseType) {
-      grunt.fail.fatal('Required release type as target (' + RELEASE_TYPES.join(', ') + ')');
-    }
+        if (!releaseType) {
+            grunt.fail.fatal('Required release type as target (' + RELEASE_TYPES.join(', ') + ')');
+        }
 
-    if (RELEASE_TYPES.indexOf(releaseType) === -1) {
-      grunt.fail.fatal('Unknown release type - use one allowed value (' + RELEASE_TYPES.join(', ') + ')');
-    }
+        if (RELEASE_TYPES.indexOf(releaseType) === -1) {
+            grunt.fail.fatal('Unknown release type - use one allowed value (' + RELEASE_TYPES.join(', ') + ')');
+        }
 
-    grunt.task.run(['bump:' + releaseType, 'publish']);
-  });
+        grunt.task.run(['bump:' + releaseType, 'publish']);
+    });
 
-  require('load-grunt-config')(grunt, {
-    configPath: [
-      path.join(process.cwd(), 'grunt', 'config'),    // tasks configurations
-      path.join(process.cwd(), 'grunt')               // tasks aliases
-    ],
-    init: true,
-    loadGruntTasks: {
-      pattern: 'grunt*'
-    }
-  });
+    require('load-grunt-config')(grunt, {
+        configPath: [
+            path.join(process.cwd(), 'grunt', 'config'),    // tasks configurations
+            path.join(process.cwd(), 'grunt')               // tasks aliases
+        ],
+        init: true,
+        loadGruntTasks: {
+            pattern: 'grunt*'
+        }
+    });
 };
